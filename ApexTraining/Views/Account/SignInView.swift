2353f78b1877eb0fafc9aff840f165763724f709
Brendan Scott <brendanscott21@gmail.com>
2023-04-18 20:15:25 -0500

    1. Modified the way the view looks to make it a little nicer

--- ApexTraining/Views/Account/SignInView.swift
+++ ApexTraining/Views/Account/SignInView.swift
@@ -29,77 +29,98 @@ struct SignInView: View {
     
     var body: some View {
         
-        VStack {
+        ZStack {
             
-            // Input Fields
-            TextField(Constants.email, text: $email)
-            if signInMode == Constants.signInMode.createAccount {
-                TextField(Constants.name, text: $name)
-            }
-            SecureField(Constants.password, text: $password)
+            // Create the box around the Sign In
+            Rectangle()
+                .frame(height: 275)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 5)
             
-            Picker(selection: $signInMode, label: Text("Placeholder")) {
-                // Sign In Option
-                Text(Constants.signIn)
-                    .tag(Constants.signInMode.signIn)
-                // Create Account Option
-                Text(Constants.createAccount)
-                    .tag(Constants.signInMode.createAccount)
-            }
-            .pickerStyle(SegmentedPickerStyle())
-            
-            // Display the error message if there is one
-            if errorMessage != nil {
-                Text(errorMessage!)
-            }
-            
-            // Sign In / Create Account Button
-            Button {
-                if signInMode == Constants.signInMode.signIn {
-                    Auth.auth().signIn(withEmail: email, password: password) { result, error in
-                        guard error == nil else {
-                            errorMessage = error!.localizedDescription
-                            return
+            VStack {
+                
+                Text("Apex Training")
+                    .font(.title)
+                
+                // Email
+                TextField(Constants.email, text: $email)
+                    .textFieldStyle(.roundedBorder)
+                    .shadow(radius: 2)
+                if signInMode == Constants.signInMode.createAccount {
+                    TextField(Constants.name, text: $name)
+                        .textFieldStyle(.roundedBorder)
+                        .shadow(radius: 2)
+                }
+                // Password
+                SecureField(Constants.password, text: $password)
+                    .textFieldStyle(.roundedBorder)
+                    .shadow(radius: 2)
+                
+                // Sign In / Create Account Picker
+                Picker(selection: $signInMode, label: Text("Placeholder")) {
+                    // Sign In Option
+                    Text(Constants.signIn)
+                        .tag(Constants.signInMode.signIn)
+                    // Create Account Option
+                    Text(Constants.createAccount)
+                        .tag(Constants.signInMode.createAccount)
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                // Display the error message if there is one
+                if errorMessage != nil {
+                    Text(errorMessage!)
+                }
+                
+                // Sign In / Create Account Button
+                Button {
+                    if signInMode == Constants.signInMode.signIn {
+                        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+                            guard error == nil else {
+                                errorMessage = error!.localizedDescription
+                                return
+                            }
+                            self.errorMessage = nil
+                            
+                            self.model.getUserData()
+                            
+                            self.model.checkSignIn()
                         }
-                        self.errorMessage = nil
-                        
-                        self.model.getUserData()
-                        
-                        self.model.checkSignIn()
+                    } else {
+                        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                            guard error == nil else {
+                                errorMessage = error!.localizedDescription
+                                return
+                            }
+                            self.errorMessage = nil
+                            
+                            // Save User
+                            let firebaseUser = Auth.auth().currentUser
+                            let db = Firestore.firestore()
+                            let ref = db.collection("users").document(firebaseUser!.uid)
+                            
+                            ref.setData(["name":name], merge: true)
+                            
+                            let user = UserService.shared.user
+                            user.name = name
+                            
+                            model.checkSignIn()
+                            
+                        }
                     }
-                } else {
-                    Auth.auth().createUser(withEmail: email, password: password) { result, error in
-                        guard error == nil else {
-                            errorMessage = error!.localizedDescription
-                            return
-                        }
-                        self.errorMessage = nil
-                        
-                        // Save User
-                        let firebaseUser = Auth.auth().currentUser
-                        let db = Firestore.firestore()
-                        let ref = db.collection("users").document(firebaseUser!.uid)
-                        
-                        ref.setData(["name":name], merge: true)
-                        
-                        let user = UserService.shared.user
-                        user.name = name
-                        
-                        model.checkSignIn()
-                        
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .foregroundColor(.red)
+                            .frame(height: 30)
+                            .cornerRadius(10)
+                        Text(buttonText)
+                            .foregroundColor(.white)
                     }
                 }
-            } label: {
-                ZStack {
-                    Rectangle()
-                        .foregroundColor(.red)
-                        .frame(height: 30)
-                        .cornerRadius(10)
-                    Text(buttonText)
-                        .foregroundColor(.white)
-                }
             }
-            
+            .padding(.horizontal, 20)
         }
         .padding(.horizontal, 20)
         
